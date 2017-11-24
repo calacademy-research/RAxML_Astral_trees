c5d8438003e33febdfad68a71c000fd8c1637f9a
Brian Simison <bsimison@tdobz.calacademy.org>
2017-11-24 09:48:56 -0800

    updated comments

--- astral_prep.sh
+++ astral_prep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#make directories for the phylip files with name of uce locus
+# Make directories for the phylip files with name of uce locus and launch R code file RCmds
 for f in uce*.nexus; do
     dir=$(basename $f .nexus)
     [ ! -d $dir ] && mkdir $dir
